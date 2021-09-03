61268a0aafaec0ec49438185d3f4d67cc86781ca
Chlamydomonos <2457242458@qq.com>
2021-09-03 21:16:17 +0800

    Modified AnimationFrame so that it would not be C in Github

--- API/src/graphic/AnimationFrame.h
+++ API/src/graphic/AnimationFrame.h
@@ -3,10 +3,13 @@
 
 #include <QPixmap>
 
-struct AnimationFrame
+class AnimationFrame
 {
+public:
 	QPixmap *pixmap;
 	int time;
+	AnimationFrame() : pixmap(nullptr), time(0) {}
+	AnimationFrame(QPixmap *_pixmap, int _time) : pixmap(_pixmap), time(_time) {}
 };
 
 #endif // !GRAPHIC__ANIMATION_FRAME_H
