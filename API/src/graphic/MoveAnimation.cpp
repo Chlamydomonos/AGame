0e640b860960ed4bd9a72c8022d8261542c89b14
Chlamydomonos <2457242458@qq.com>
2021-09-08 21:19:27 +0800

    Fixed MoveAnimation

--- API/src/graphic/MoveAnimation.cpp
+++ API/src/graphic/MoveAnimation.cpp
@@ -14,9 +14,17 @@ MoveAnimation::MoveAnimation(const QPointF &_from, const QPointF &_to, int __dur
 
 void MoveAnimation::updateCurrentTime(int currentTime)
 {
-	currentTime %= duration();
-	double rate = (double)currentTime / (double)duration();
-	double x = from.x() * (1 - rate) + to.x() * rate;
-	double y = from.y() * (1 - rate) + to.y() * rate;
-	sprite->setPos(x, y);
+	if (state() == State::Running)
+	{
+		if (currentTime == 0 || currentTime % duration() != 0)
+		{
+			currentTime %= duration();
+			double rate = (double)currentTime / (double)duration();
+			double x = from.x() * (1 - rate) + to.x() * rate;
+			double y = from.y() * (1 - rate) + to.y() * rate;
+			sprite->setPos(x, y);
+		}
+		else
+			sprite->setPos(to);
+	}
 }
